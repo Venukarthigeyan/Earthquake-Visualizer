2195903e1e599b7f00dc17fa7dfaaffc97f305d1
VENUKARTHIGEYAN G <venu@Venukarthigeyans-MacBook-Air.local>
2025-09-03 21:58:52 +0530

    deploy server

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     allowedHosts: [
-      'mlhcq7-5173.csb.app',
       '.csb.app',
       '.stackblitz.io',
+      /^.*-\d+\.csb\.app$/, // regex pattern for CodeSandbox URLs
       'localhost'
     ],
     host: true
