d8f2d22cf52b7feca5a6d59ebd52da0246504b66
VENUKARTHIGEYAN G <venu@Venukarthigeyans-MacBook-Air.local>
2025-09-03 21:51:59 +0530

    deploy server

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      '.csb.app',        // allow CodeSandbox previews
+      '.stackblitz.io'   // allow StackBlitz previews
+    ],
+    host: true           // ensures it binds to 0.0.0.0
+  }
 })
